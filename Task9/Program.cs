e2920e9c28875ec51abeda7c3088a1efac49cfc9
Ravil <karimra@mail.ru>
2022-11-28 19:38:08 +0300

    task9

---
+++ Task9/Program.cs
@@ -0,0 +1,11 @@
+﻿ Random rand = new Random();
+int number = rand.Next(10, 100);
+Console.WriteLine(number);
+
+int digitOnes = number % 10;
+int digitTens = number / 10;
+int max = digitTens;
+
+if (digitOnes > digitTens) max = digitOnes;
+
+Console.WriteLine("Наибольшая цифра числа: " + max);
