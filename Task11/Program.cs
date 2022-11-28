8c45691b0cd6229551c8bf219400a49bd95001af
Ravil <karimra@mail.ru>
2022-11-28 20:27:15 +0300

    nask17

---
+++ Task11/Program.cs
@@ -0,0 +1,15 @@
+﻿
+/*
+Random rand = new Random();
+int number = rand.Next(100, 300);
+Console.WriteLine(number);
+
+int digitOnes = number % 10;    // 731 % 10 =1
+int digitTens = number / 10 % 10;  // 731 /10%10 = 73%10 =3
+int digitHun = number / 100;     // 731/100 =7;
+int result = (digitHun * 10) + digitOnes;
+
+//if (digitOnes > digitTens) max = digitOnes;
+
+Console.WriteLine(result);
+*/
