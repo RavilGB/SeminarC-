2913037e93802e08e6692f6da34631cd0c16009a
Ravil <karimra@mail.ru>
2022-11-28 21:30:43 +0300

    2122

---
+++ Task21/Program.cs
@@ -0,0 +1,84 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hello, World!");
+AB = √(xb - xa)2 + (yb - ya)2
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+A (3,6); B (2,1) -> 5,09 
+A (7,-5); B (1,-1) -> 7,21
+*/
+/*int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result != 0)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Введите корректное число");
+        }
+    }
+
+    return result;
+}
+
+int x1 = GetNumber("ввод x1" );
+int x2 = GetNumber("ввод x2" );
+int y1 = GetNumber("ввод y1" );
+int y2 = GetNumber("ввод y2" );
+
+double sum1 = Math.Pow((x2 - x1), 2);
+double sum2 = Math.Pow((y2 - y1), 2);
+
+double result = Math.Sqrt(sum1 + sum2);
+
+Console.WriteLine(result);
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+от 1 до N.
+
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4
+*/
+/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+от 1 до N.
+
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result >= 1)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Введите целое число больше 0");
+        }
+    }
+
+    return result;
+}
+
+int lin = GetNumber("Введите число");
+for(int i=1;i<=lin;i++)
+{
+    Console.Write($" {Math.Pow(i,2)}, ");
+}
