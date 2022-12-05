736f1e3d3bc9839e1993d8b6d5c069c369f7df26
Ravil <karimra@mail.ru>
2022-12-05 21:54:34 +0300

    array

--- Task31/Program.cs
+++ Task31/Program.cs
@@ -158,7 +158,100 @@ Console.WriteLine("введите чиcло: ");
 int num = Convert.ToInt32(Console.ReadLine() ?? "");
 Console.WriteLine(IsExist(array,num));
 */
+//Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+//Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+//[5, 18, 123, 6, 2] -> 1
+//[1, 2, 3, 6, 2] -> 0
+//[10, 11, 12, 13, 14] -> 5
+/*
+int[] GetArray(int length)
+{
+    Random rnd = new Random();
+    int [] array = new int [length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(-100,300);
+    }
+    return array;
+}
+
+int Count(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >=10 && array[i]<= 99) count++;
+    }
+    return count;
+}
+
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
 
 
+Console.WriteLine("Ведите размер массива");
+int[] array = GetArray(int.Parse(Console.ReadLine()));
+
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"{Count(array)}");
+*/
+//Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+//[1 2 3 4 5] -> 5 8 3
+//[6 7 3 6] -> 36 21
 
 
+int [] GetArray(int length)
+{
+    Random rnd = new Random();
+    int [] array = new int [length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(1, 5);
+    }
+    return array;
+}
+
+int[] NewArray(int[] array)
+{
+    int[] arr;
+    int len = 0;
+    if (array.Length%2 == 0) 
+    {
+        len = array.Length/2;
+        arr = new int [len];
+    }
+    else 
+    {
+        len = array.Length/2 + 1;
+        arr = new int [len];
+        arr[len-1] = array[len];
+    }
+    
+    for (int i = 0; i < array.Length/2; i++)
+    {
+        arr[i] = array[i]*array[array.Length - i - 1];
+    }
+    return arr;
+}
+
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+
+Console.WriteLine("Ведите размер массива");
+int[] array = GetArray(int.Parse(Console.ReadLine()));
+PrintArray(array);
+int [] arr = NewArray(array);
+Console.WriteLine();
+PrintArray(arr);
